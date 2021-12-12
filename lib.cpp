df69564d1d9031b63aa65f038ac055ac7faf6acd
abishekmahendran <91681257+abishekmahendran@users.noreply.github.com>
2021-12-12 01:27:44 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+#include "lib.h"
+#include <cstring>
 
+
+bool nomi(char lista [10][20], char nome[1][20], int &posizione){
+  
+  for (int i=0; i<10; i++){
+    if (strcmp (lista [i], nome [0])== 0){
+      posizione = 1;
+      return true;
+    }
+  }
+  return false;
+}
+  
