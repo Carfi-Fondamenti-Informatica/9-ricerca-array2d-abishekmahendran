fb7fc0b86c64cc752ea9ab8909c70866d67ccb4b
abishekmahendran <91681257+abishekmahendran@users.noreply.github.com>
2021-12-12 01:43:34 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -5,8 +5,8 @@
 bool nomi(char lista [10][20], char nome[1][20], int &posizione){
   
   for (int i=0; i<10; i++){
-    if (strcmp (lista [i], nome [0])== 0){
-      posizione = 1;
+    if (strcmp (lista [i], nome [0]) == 0){
+      posizione = i;
       return true;
     }
   }
