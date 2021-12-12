210745477bbf587ca6fcc91e676121fa7abb6bde
abishekmahendran <91681257+abishekmahendran@users.noreply.github.com>
2021-12-12 01:39:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  int posizione;
+  char lista[10][20], nome[1][20];
+  for (int i=0; i<10; i++){
+    cin >> lista[0];
+  }
+  cin >> nome[0];
+  if (nomi(lista, nome, posizione)){
+    cout<< posizione <<endl;
+  }else{
+    cout<< "non presente" <<endl;
+  }
   return 0;
 }
