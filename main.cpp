a54fca86a68eee6d734ee39b4422aa20a36655a1
abishekmahendran <91681257+abishekmahendran@users.noreply.github.com>
2021-12-12 01:39:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(){
   int posizione;
   char lista[10][20], nome[1][20];
   for (int i=0; i<10; i++){
-    cin >> lista[0];
+    cin >> lista[i];
   }
   cin >> nome[0];
   if (nomi(lista, nome, posizione)){
